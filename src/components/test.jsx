a84a420af4852ac79936fa0a3c184f0a6f034e81
gyeolhwi <gyeori0626@gmail.com>
2024-10-02 11:25:17 +0900

    first commit

---
+++ src/components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */
+import * as s from './style';
+
+function test(props) {
+    return (
+        <div >
+            
+        </div>
+    );
+}
+
+export default test;
