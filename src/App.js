623ec99146a8c13a1e041289d747764b4a2334ff
qorditl1248 <qorditl1248@naver.com>
2024-10-02 12:43:09 +0900

    진행 중..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Global } from "@emotion/react";
 import { reset } from "./styles/common";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Global styles={reset} />
-
+        <Routes>
+          <Route/>
+          <Route/>
+          <Route/>
+        </Routes>
     </>
   );
 }
