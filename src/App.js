d7d30f04c04fd743982ff6a30e22f1a4ca5c2871
qorditl1248 <qorditl1248@naver.com>
2024-10-07 16:48:11 +0900

    메인화면설계 및 수정 완료

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import MainTop from "./components/MainTop/MainTop";
 import MainTopBar from "./components/MainTopBar/MainTopBar";
 import TestPage from "./TestPage";
+import MainFooter from "./components/MainFooter/MainFooter";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/test1" element={<MainTop />} />
         <Route path="/test2" element={<MainTopBar />} />
+        <Route path="/test3" element={<MainFooter />} />
         <Route path="/" element={<TestPage/>} />
       </Routes>
     </>
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,11 +1,12 @@
 // 대표 색상's
-// 사용할 떄 - color: ${COLORS.(value값들)};
+// 사용할 떄 - color: ${COLORS.main}; 이런 식으로
 
 export const COLORS = {
   main: "#036635",
   mainBackground: "#ffffff",
   lineColor: "#333333",
   buttonColor: "#459661",
-  buttonFontColor: "#fafafa"
+  buttonFontColor: "#fafafa",
+  mainLineColor: "#fafafa", 
 }
 
